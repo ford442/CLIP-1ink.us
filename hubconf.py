00589c6dd2d7a670e57e2ce6ed55897dfdefaed3
Noah Cohn <noahc42@gmail.com>
2022-08-21 02:15:51 -0700

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,16 +1,11 @@
 from clip.clip import tokenize as _tokenize, load as _load, available_models as _available_models
 import re
 import string
-
 dependencies=["torch", "torchvision", "ftfy", "regex", "tqdm"]
-
-# For compatibility (cannot include special characters in function name)
 model_functions={ model: re.sub(f'[{string.punctuation}]', '_', model) for model in _available_models()}
-
 def _create_hub_entrypoint(model):
     def entrypoint(**kwargs):      
         return _load(model, **kwargs)
-    
     entrypoint.__doc__=f"""Loads the {model} CLIP model
 
         Parameters
@@ -33,10 +28,7 @@ def _create_hub_entrypoint(model):
             A torchvision transform that converts a PIL image into a tensor that the returned model can take as its input
         """
     return entrypoint
-
 def tokenize():
     return _tokenize
-
 _entrypoints={model_functions[model]: _create_hub_entrypoint(model) for model in _available_models()}
-
 globals().update(_entrypoints)
