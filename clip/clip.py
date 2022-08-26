9eb5db59eb4e306377c340c040ff20e363d7dd8a
Noah Cohn <noahc42@gmail.com>
2022-08-26 01:26:50 -0700

    Update clip.py

--- clip/clip.py
+++ clip/clip.py
@@ -71,10 +71,10 @@ def load(fp16bit,sIze,name):
     model_path=name
     with open(model_path, 'rb') as opened_file:
         try:
-            model=torch.jit.load(opened_file).eval()
+            model=torch.jit.load(opened_file, map_location='cuda:0').eval()
             state_dict=None
         except RuntimeError:
-            state_dict=torch.load(opened_file)
+            state_dict=torch.load(opened_file, map_location="cuda:0")
     if not jit:
         model=build_model(fp16bit,state_dict or model.state_dict()).to("cuda:0")
         if str(device) == "cpu":
