8c5cc35a8d2237ff58250d55403c99557e6b5529
Noah Cohn <noahc42@gmail.com>
2022-08-25 22:10:47 -0700

    Update model.py

--- clip/model.py
+++ clip/model.py
@@ -180,7 +180,7 @@ class Transformer16(nn.Module):
         super().__init__()
         self.width=width
         self.layers=layers
-        self.resblocks=nn.Sequential(*[ResidualAttentionBlock(width, heads, attn_mask) for _ in range(layers)])
+        self.resblocks=nn.Sequential(*[ResidualAttentionBlock16(width, heads, attn_mask) for _ in range(layers)])
     def forward(self, x: torch.Tensor):
         return self.resblocks(x)
 class VisionTransformer(nn.Module):
@@ -194,6 +194,33 @@ class VisionTransformer(nn.Module):
         self.positional_embedding=nn.Parameter(scale * torch.randn((input_resolution // patch_size) ** 2 + 1, width))
         self.ln_pre=LayerNorm(width)
         self.transformer=Transformer(width, layers, heads)
+        self.ln_post=LayerNorm(width)
+        self.proj=nn.Parameter(scale * torch.randn(width, output_dim))
+    def forward(self, x: torch.Tensor):
+        x=self.conv1(x)  # shape=[*, width, grid, grid]
+        x=x.reshape(x.shape[0], x.shape[1], -1)  # shape=[*, width, grid ** 2]
+        x=x.permute(0, 2, 1)  # shape=[*, grid ** 2, width]
+        x=torch.cat([self.class_embedding.to(x.dtype) + torch.zeros(x.shape[0], 1, x.shape[-1], dtype=x.dtype, device=x.device), x], dim=1)  # shape=[*, grid ** 2 + 1, width]
+        x=x + self.positional_embedding.to(x.dtype)
+        x=self.ln_pre(x)
+        x=x.permute(1, 0, 2)  # NLD -> LND
+        x=self.transformer(x)
+        x=x.permute(1, 0, 2)  # LND -> NLD
+        x=self.ln_post(x[:, 0, :])
+        if self.proj is not None:
+            x=x @ self.proj
+        return x
+class VisionTransformer16(nn.Module):
+    def __init__(self, input_resolution: int, patch_size: int, width: int, layers: int, heads: int, output_dim: int):
+        super().__init__()
+        self.input_resolution=input_resolution
+        self.output_dim=output_dim
+        self.conv1=nn.Conv2d(in_channels=3, out_channels=width, kernel_size=patch_size, stride=patch_size, bias=False)
+        scale=width ** -0.5
+        self.class_embedding=nn.Parameter(scale * torch.randn(width))
+        self.positional_embedding=nn.Parameter(scale * torch.randn((input_resolution // patch_size) ** 2 + 1, width))
+        self.ln_pre=LayerNorm(width)
+        self.transformer=Transformer16(width, layers, heads)
         self.ln_post=LayerNorm(width)
         self.proj=nn.Parameter(scale * torch.randn(width, output_dim))
     def forward(self, x: torch.Tensor):
