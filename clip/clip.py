90cf0ad19524e5b6ddb1f5111243e1d6e6404620
Noah Cohn <noahc42@gmail.com>
2022-08-30 07:00:40 -0700

    Update clip.py

--- clip/clip.py
+++ clip/clip.py
@@ -71,14 +71,14 @@ def load(fp16bit,sIze,name):
     jit=False
     model_path=name
     with open(model_path, 'rb') as opened_file:
-        try:
-            model=torch.jit.load(opened_file, map_location=device).eval()
-            state_dict=None
-        except RuntimeError:
-            if jit:
-                warnings.warn(f"File {model_path} is not a JIT archive. Loading as a state dict instead")
-                jit=False
-            state_dict=torch.load(opened_file,map_location=device)
+        #try:
+       #     model=torch.jit.load(opened_file, map_location=device).eval()
+        #    state_dict=None
+       # except RuntimeError:
+        #    if jit:
+        #        warnings.warn(f"File {model_path} is not a JIT archive. Loading as a state dict instead")
+       #         jit=False
+        state_dict=torch.load(opened_file,map_location=device)
     if not jit:
         model=build_model(fp16bit,state_dict or model.state_dict()).to(device)
         if str(device)=="cpu":
