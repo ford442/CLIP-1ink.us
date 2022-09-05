56458217d8deac529888cd86cc0c007c84fea279
Noah Cohn <noahc42@gmail.com>
2022-09-05 12:09:29 -0700

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,7 +1,7 @@
 from clip.clip import tokenize as _tokenize, load as _load, available_models as _available_models
 import re
 import string
-dependencies=["torch", "torchvision", "ftfy", "regex", "tqdm"]
+
 model_functions={ model: re.sub(f'[{string.punctuation}]', '_', model) for model in _available_models()}
 def _create_hub_entrypoint(model):
     def entrypoint(**kwargs):      
