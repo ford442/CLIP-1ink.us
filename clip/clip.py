69d19aa304fe96636077cd79b3f27aace1b72c1a
Noah Cohn <noahc42@gmail.com>
2022-09-08 07:26:19 -0700

    Update clip.py

--- clip/clip.py
+++ clip/clip.py
@@ -69,26 +69,25 @@ def _transform(n_px):
 def available_models() -> List[str]:
     return list(_MODELS.keys())
 def load(fp16bit,sIze,name):
-    devicec=torch.device("cuda:0")
-    deviceg=torch.device("cuda:0")
+    device=torch.device("cuda:0")
     jit=False
     model_path=name
     with open(model_path, 'rb') as opened_file:
         try:
-            model=torch.jit.load(opened_file, map_location=devicec)
+            model=torch.jit.load(opened_file, map_location=device)
             state_dict=None
         except RuntimeError:
             if jit:
                 warnings.warn(f"File {model_path} is not a JIT archive. Loading as a state dict instead")
                 jit=False
-            state_dict=torch.load(opened_file,map_location=devicec)
+            state_dict=torch.load(opened_file,map_location=device)
     if not jit:
         model=build_model(fp16bit,state_dict or model.state_dict())
         if str(device)=="cpu":
             model.float()
         return model,_transform(sIze)
     return model,_transform(sIze)
-    device_holder=torch.jit.trace(lambda:torch.ones([]).to(deviceg),example_inputs=[])
+    device_holder=torch.jit.trace(lambda:torch.ones([]).to(device),example_inputs=[])
     device_node=[n for n in device_holder.graph.findAllNodes("prim::Constant") if "Device" in repr(n)][-1]
     def patch_device(module):
         try:
@@ -128,7 +127,6 @@ def load(fp16bit,sIze,name):
         model.float()
     return model,_transform(sIze)
 
-@cache(maxsize=40)
 def tokenize(texts:Union[str,List[str]],context_length:int=77,truncate:bool=False)->Union[torch.IntTensor,torch.LongTensor]:
     if isinstance(texts,str):
         texts=[texts]
