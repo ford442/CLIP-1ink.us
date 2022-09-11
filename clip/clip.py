e62f104133d67a7b311095d561ac319b2792661d
Noah Cohn <noahc42@gmail.com>
2022-09-11 05:24:08 -0700

    Update clip.py

--- clip/clip.py
+++ clip/clip.py
@@ -1,24 +1,24 @@
-import hashlib
-import os
-import urllib
-import warnings
-from typing import Any,Union,List
-from pkg_resources import packaging
-import torch
-from PIL import Image
-from torchvision.transforms import Compose,Resize,CenterCrop,ToTensor,Normalize
-from tqdm import tqdm
-from .model import build_model
-from .simple_tokenizer import SimpleTokenizer as _Tokenizer
+import hashlib;
+import os;
+import urllib;
+import warnings;
+from typing import Any,Union,List;
+from pkg_resources import packaging;
+import torch;
+from PIL import Image;
+from torchvision.transforms import Compose,Resize,CenterCrop,ToTensor,Normalize;
+from tqdm import tqdm;
+from .model import build_model;
+from .simple_tokenizer import SimpleTokenizer as _Tokenizer;
 try:
-    from torchvision.transforms import InterpolationMode
-    BICUBIC=InterpolationMode.BICUBIC
+    from torchvision.transforms import InterpolationMode;
+    BICUBIC=InterpolationMode.BICUBIC;
 except ImportError:
-    BICUBIC=Image.BICUBIC
+    BICUBIC=Image.BICUBIC;
 if packaging.version.parse(torch.__version__)<packaging.version.parse("1.7.1"):
-    warnings.warn("PyTorch version 1.7.1 or higher is recommended")
-__all__=["available_models","load","tokenize"]
-_tokenizer=_Tokenizer()
+    warnings.warn("PyTorch version 1.7.1 or higher is recommended");
+__all__=["available_models","load","tokenize"];
+_tokenizer=_Tokenizer();
 _MODELS={
     "RN50": "https://openaipublic.azureedge.net/clip/models/afeb0e10f9e5a86da6080e35cf09123aca3b358a0c3e3b6c78a7b63bc04b6762/RN50.pt",
     "RN101": "https://openaipublic.azureedge.net/clip/models/8fa8567bab74a42d41c5915025a8e4538c3bdbe8804a470a72f30b0d94fab599/RN101.pt",
@@ -28,119 +28,117 @@ _MODELS={
     "ViT-B/32": "https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt",
     "ViT-B/16": "https://openaipublic.azureedge.net/clip/models/5806e77cd80f8b59890b7e101eabd078d9fb84e6937f9e85e4ecb61988df416f/ViT-B-16.pt",
     "ViT-L/14": "https://openaipublic.azureedge.net/clip/models/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt",
-    "ViT-L/14@336px": "https://openaipublic.azureedge.net/clip/models/3035c92b350959924f9f00213499208652fc7ea050643e8b385c2dac08641f02/ViT-L-14-336px.pt",
-}
+    "ViT-L/14@336px": "https://openaipublic.azureedge.net/clip/models/3035c92b350959924f9f00213499208652fc7ea050643e8b385c2dac08641f02/ViT-L-14-336px.pt",};
 def _download(url:str,root:str):
-    os.makedirs(root,exist_ok=True)
-    filename=os.path.basename(url)
-    expected_sha256=url.split("/")[-2]
-    download_target=os.path.join(root,filename)
+    os.makedirs(root,exist_ok=True);
+    filename=os.path.basename(url);
+    expected_sha256=url.split("/")[-2];
+    download_target=os.path.join(root,filename);
     if os.path.exists(download_target) and not os.path.isfile(download_target):
-        raise RuntimeError(f"{download_target} exists and is not a regular file")
+        raise RuntimeError(f"{download_target} exists and is not a regular file");
     if os.path.isfile(download_target):
         if hashlib.sha256(open(download_target,"rb").read()).hexdigest()==expected_sha256:
-            return download_target
+            return download_target;
         else:
-            warnings.warn(f"{download_target} exists, but the SHA256 checksum does not match; re-downloading the file")
+            warnings.warn(f"{download_target} exists, but the SHA256 checksum does not match; re-downloading the file");
     with urllib.request.urlopen(url) as source,open(download_target,"wb") as output:
         with tqdm(total=int(source.info().get("Content-Length")),ncols=80,unit='iB',unit_scale=True,unit_divisor=1024) as loop:
             while True:
-                buffer=source.read(8192)
+                buffer=source.read(8192);
                 if not buffer:
-                    break
-                output.write(buffer)
-                loop.update(len(buffer))
+                    break;
+                output.write(buffer);
+                loop.update(len(buffer));
     if hashlib.sha256(open(download_target,"rb").read()).hexdigest()!=expected_sha256:
-        raise RuntimeError(f"Model has been downloaded but the SHA256 checksum does not not match")
-    return download_target
+        raise RuntimeError(f"Model has been downloaded but the SHA256 checksum does not not match");
+    return download_target;
 def _convert_image_to_rgb(image):
-    return image.convert("RGB")
+    return image.convert("RGB");
 def _transform(n_px):
     return Compose([
         Resize(n_px, interpolation=BICUBIC),
         CenterCrop(n_px),
         _convert_image_to_rgb,
         ToTensor(),
-        Normalize((0.48145466,0.4578275,0.40821073),(0.26862954,0.26130258,0.27577711)),
-    ])
+        Normalize((0.48145466,0.4578275,0.40821073),(0.26862954,0.26130258,0.27577711)),]);
 def available_models() -> List[str]:
-    return list(_MODELS.keys())
+    return list(_MODELS.keys());
 def load(fp16bit,sIze,name):
-    device=torch.device("cuda:0")
-    device_CPU=torch.device("cpu")
-    jit=False
-    model_path=name
+    device=torch.device("cuda:0");
+    device_CPU=torch.device("cpu");
+    jit=False;
+    model_path=name;
     with open(model_path, 'rb') as opened_file:
         try:
-            model=torch.jit.load(opened_file, map_location=device)
-            state_dict=None
+            model=torch.jit.load(opened_file, map_location=device_CPU);
+            state_dict=None;
         except RuntimeError:
             if jit:
-                warnings.warn(f"File {model_path} is not a JIT archive. Loading as a state dict instead")
-                jit=False
-            state_dict=torch.load(opened_file,map_location=device)
+                warnings.warn(f"File {model_path} is not a JIT archive. Loading as a state dict instead");
+                jit=False;
+            state_dict=torch.load(opened_file,map_location=device_CPU);
     if not jit:
-        model=build_model(fp16bit,state_dict or model.state_dict())
+        model=build_model(fp16bit,state_dict or model.state_dict());
         if str(device)=="cpu":
-            model.float()
-        return model,_transform(sIze)
-    return model,_transform(sIze)
-    device_holder=torch.jit.trace(lambda:torch.ones([]).to(torch.device(device),non_blocking=True),example_inputs=[])
-    device_node=[n for n in device_holder.graph.findAllNodes("prim::Constant") if "Device" in repr(n)][-1]
+            model.float();
+        return model,_transform(sIze);
+    return model,_transform(sIze);
+    device_holder=torch.jit.trace(lambda:torch.ones([]).to(torch.device(device),non_blocking=True),example_inputs=[]);
+    device_node=[n for n in device_holder.graph.findAllNodes("prim::Constant") if "Device" in repr(n)][-1];
     def patch_device(module):
         try:
-            graphs=[module.graph] if hasattr(module,"graph") else []
+            graphs=[module.graph] if hasattr(module,"graph") else [];
         except RuntimeError:
-            graphs=[]
+            graphs=[];
         if hasattr(module,"forward1"):
-            graphs.append(module.forward1.graph)
+            graphs.append(module.forward1.graph);
         for graph in graphs:
             for node in graph.findAllNodes("prim::Constant"):
                 if "value" in node.attributeNames() and str(node["value"]).startswith("cuda"):
-                    node.copyAttributes(device_node)
+                    node.copyAttributes(device_node);
     if fp16bit==False:
-        model.apply(patch_device)
-        patch_device(model.encode_image)
-        patch_device(model.encode_text)
+        model.apply(patch_device);
+        patch_device(model.encode_image);
+        patch_device(model.encode_text);
     if str(device)=="cpu":
-        float_holder=torch.jit.trace(lambda:torch.ones([]).float(),example_inputs=[])
-        float_input=list(float_holder.graph.findNode("aten::to").inputs())[1]
-        float_node=float_input.node()
+        float_holder=torch.jit.trace(lambda:torch.ones([]).float(),example_inputs=[]);
+        float_input=list(float_holder.graph.findNode("aten::to").inputs())[1];
+        float_node=float_input.node();
         def patch_float(module):
             try:
-                graphs=[module.graph] if hasattr(module,"graph") else []
+                graphs=[module.graph] if hasattr(module,"graph") else [];
             except RuntimeError:
-                graphs=[]
+                graphs=[];
             if hasattr(module,"forward1"):
-                graphs.append(module.forward1.graph)
+                graphs.append(module.forward1.graph);
             for graph in graphs:
                 for node in graph.findAllNodes("aten::to"):
-                    inputs=list(node.inputs())
+                    inputs=list(node.inputs());
                     for i in [1, 2]:
                         if inputs[i].node()["value"]==5:
-                            inputs[i].node().copyAttributes(float_node)
-        model.apply(patch_float)
-        patch_float(model.encode_image)
-        patch_float(model.encode_text)
-        model.float()
-    return model,_transform(sIze)
+                            inputs[i].node().copyAttributes(float_node);
+        model.apply(patch_float);
+        patch_float(model.encode_image);
+        patch_float(model.encode_text);
+        model.float();
+    return model,_transform(sIze);
 
 def tokenize(texts:Union[str,List[str]],context_length:int=77,truncate:bool=False)->Union[torch.IntTensor,torch.LongTensor]:
     if isinstance(texts,str):
-        texts=[texts]
-    sot_token=_tokenizer.encoder["<|startoftext|>"]
-    eot_token=_tokenizer.encoder["<|endoftext|>"]
-    all_tokens=[[sot_token]+_tokenizer.encode(text)+[eot_token]for text in texts]
+        texts=[texts];
+    sot_token=_tokenizer.encoder["<|startoftext|>"];
+    eot_token=_tokenizer.encoder["<|endoftext|>"];
+    all_tokens=[[sot_token]+_tokenizer.encode(text)+[eot_token]for text in texts];
     if packaging.version.parse(torch.__version__)<packaging.version.parse("1.8.0"):
-        result=torch.zeros(len(all_tokens),context_length,dtype=torch.long)
+        result=torch.zeros(len(all_tokens),context_length,dtype=torch.long);
     else:
-        result=torch.zeros(len(all_tokens),context_length,dtype=torch.int64)
+        result=torch.zeros(len(all_tokens),context_length,dtype=torch.int64);
     for i, tokens in enumerate(all_tokens):
         if len(tokens)>context_length:
             if truncate:
-                tokens=tokens[:context_length]
-                tokens[-1]=eot_token
+                tokens=tokens[:context_length];
+                tokens[-1]=eot_token;
             else:
-                raise RuntimeError(f"Input {texts[i]} is too long for context length {context_length}")
-        result[i,:len(tokens)]=torch.tensor(tokens)
-    return result
+                raise RuntimeError(f"Input {texts[i]} is too long for context length {context_length}");
+        result[i,:len(tokens)]=torch.tensor(tokens);
+    return result;
