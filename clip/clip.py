adf47e54ae6d203d830262d7b57c8e33cf987678
Noah Cohn <noahc42@gmail.com>
2022-09-14 14:51:31 -0700

    Update clip.py

--- clip/clip.py
+++ clip/clip.py
@@ -67,27 +67,20 @@ def load(fp16bit,sIze,name,tjit=False):
     device=torch.device("cuda:0");
     device_CPU=torch.device("cpu");
     model_path=name;
-    if tjit==True:
-        with open(model_path, 'rb') as opened_file:
-            try:
-                model=torch.jit.load(opened_file, map_location=None);
-                state_dict=None;
-            except RuntimeError:
-                if jit:
-                    warnings.warn(f"File {model_path} is not a JIT archive. Loading as a state dict instead");
-                    jit=False;
-                state_dict=torch.load(opened_file,map_location=None);
-        if not jit:
-            model=build_model(fp16bit,state_dict or model.state_dict());
-            if str(device)=="cpu":
-                model.float();
-            return model,_transform(model.visual.input_resolution);
-    if tjit==False:
-        with open(model_path, 'rb') as opened_file:
-            state_dict=torch.load(opened_file,map_location=None);
-            model=build_model(fp16bit,state_dict or model.state_dict());
-            return model,_transform(model.visual.input_resolution);
-    
+    with open(model_path, 'rb') as opened_file:
+        try:
+            model=torch.jit.load(opened_file, map_location=device if tjit else None).eval()
+            state_dict=None
+        except RuntimeError:
+            if jit:
+                warnings.warn(f"File {model_path} is not a JIT archive. Loading as a state dict instead")
+                jit=False
+            state_dict=torch.load(opened_file, map_location=None)
+    if not tjit:
+        model=build_model(state_dict or model.state_dict()) #.to(device)
+        if str(device) == "cpu":
+            model.float()
+        return model, _transform(model.visual.input_resolution)
     device_holder=torch.jit.trace(lambda:torch.ones([]).to(torch.device(device)),example_inputs=[]);
     device_node=[n for n in device_holder.graph.findAllNodes("prim::Constant") if "Device" in repr(n)][-1];
     def patch_device(module):
