3f770a1db34fe705d8f4631a8f68329375746547
Noah Cohn <noahc42@gmail.com>
2022-09-15 18:07:27 -0700

    Update model.py

--- clip/model.py
+++ clip/model.py
@@ -118,7 +118,12 @@ class LayerNorm(nn.LayerNorm):
 class LayerNorm16(nn.LayerNorm):
     def forward(self,x:torch.Tensor):
         orig_type=x.dtype
-        ret=super().forward(x.type(torch.half))
+        ret=super().forward(x.type(torch.float16))
+        return ret.type(orig_type)
+class LayerNorm64(nn.LayerNorm):
+    def forward(self,x:torch.Tensor):
+        orig_type=x.dtype
+        ret=super().forward(x.type(torch.float64))
         return ret.type(orig_type)
 class QuickGELU(nn.Module):
     def forward(self,x:torch.Tensor):
@@ -153,6 +158,21 @@ class ResidualAttentionBlock16(nn.Module):
         x=x+self.attention(self.ln_1(x))
         x=x+self.mlp(self.ln_2(x))
         return x
+class ResidualAttentionBlock64(nn.Module):
+    def __init__(self,d_model:int,n_head:int,attn_mask:torch.Tensor=None):
+        super().__init__()
+        self.attn=nn.MultiheadAttention(d_model,n_head)
+        self.ln_1=LayerNorm64(d_model)
+        self.mlp=nn.Sequential(OrderedDict([("c_fc",nn.Linear(d_model,d_model*4)),("gelu",QuickGELU()),("c_proj",nn.Linear(d_model*4,d_model))]))
+        self.ln_2=LayerNorm64(d_model)
+        self.attn_mask=attn_mask
+    def attention(self,x:torch.Tensor):
+        self.attn_mask=self.attn_mask.to(dtype=x.dtype,device=x.device) if self.attn_mask is not None else None
+        return self.attn(x,x,x,need_weights=False,attn_mask=self.attn_mask)[0]
+    def forward(self,x:torch.Tensor):
+        x=x+self.attention(self.ln_1(x))
+        x=x+self.mlp(self.ln_2(x))
+        return x
 class Transformer(nn.Module):
     def __init__(self,width:int,layers:int,heads:int,attn_mask:torch.Tensor=None):
         super().__init__()
@@ -167,6 +187,14 @@ class Transformer16(nn.Module):
         self.width=width
         self.layers=layers
         self.resblocks=nn.Sequential(*[ResidualAttentionBlock16(width,heads,attn_mask) for _ in range(layers)])
+    def forward(self,x:torch.Tensor):
+        return self.resblocks(x)
+class Transformer64(nn.Module):
+    def __init__(self,width: int,layers: int,heads: int,attn_mask: torch.Tensor=None):
+        super().__init__()
+        self.width=width
+        self.layers=layers
+        self.resblocks=nn.Sequential(*[ResidualAttentionBlock64(width,heads,attn_mask) for _ in range(layers)])
     def forward(self,x:torch.Tensor):
         return self.resblocks(x)
 class VisionTransformer(nn.Module):
@@ -196,6 +224,7 @@ class VisionTransformer(nn.Module):
         if self.proj is not None:
             x=x @ self.proj
         return x
+
 class VisionTransformer16(nn.Module):
     def __init__(self,input_resolution:int,patch_size:int,width:int,layers:int,heads:int,output_dim:int):
         super().__init__()
@@ -223,7 +252,36 @@ class VisionTransformer16(nn.Module):
         if self.proj is not None:
             x=x @ self.proj
         return x
-class CLIP(nn.Module):
+
+class VisionTransformer64(nn.Module):
+    def __init__(self,input_resolution:int,patch_size:int,width:int,layers:int,heads:int,output_dim:int):
+        super().__init__()
+        self.input_resolution=input_resolution
+        self.output_dim=output_dim
+        self.conv1=nn.Conv2d(in_channels=3,out_channels=width,kernel_size=patch_size,stride=patch_size,bias=False)
+        scale=width**-0.5
+        self.class_embedding=nn.Parameter(scale*torch.randn(width))
+        self.positional_embedding=nn.Parameter(scale*torch.randn((input_resolution//patch_size)**2+1,width))
+        self.ln_pre=LayerNorm64(width)
+        self.transformer=Transformer64(width,layers,heads)
+        self.ln_post=LayerNorm64(width)
+        self.proj=nn.Parameter(scale*torch.randn(width,output_dim))
+    def forward(self,x:torch.Tensor):
+        x=self.conv1(x)
+        x=x.reshape(x.shape[0],x.shape[1],-1)
+        x=x.permute(0,2,1)
+        x=torch.cat([self.class_embedding.to(x.dtype)+torch.zeros(x.shape[0],1,x.shape[-1],dtype=x.dtype,device=x.device),x],dim=1)
+        x=x + self.positional_embedding.to(x.dtype)
+        x=self.ln_pre(x)
+        x=x.permute(1,0,2)
+        x=self.transformer(x)
+        x=x.permute(1,0,2)
+        x=self.ln_post(x[:,0,:])
+        if self.proj is not None:
+            x=x @ self.proj
+        return x
+    
+    class CLIP(nn.Module):
     def __init__(self,
                  embed_dim:int,
                  image_resolution:int,
@@ -421,6 +479,107 @@ class CLIP16(nn.Module):
         logits_per_image=logit_scale * image_features @ text_features.t()
         logits_per_text=logits_per_image.t()
         return logits_per_image,logits_per_text
+
+class CLIP64(nn.Module):
+    def __init__(self,
+                 embed_dim:int,
+                 image_resolution:int,
+                 vision_layers:Union[Tuple[int,int,int,int],int],
+                 vision_width:int,
+                 vision_patch_size:int,
+                 context_length:int,
+                 vocab_size:int,
+                 transformer_width:int,
+                 transformer_heads:int,
+                 transformer_layers:int
+                 ):
+        super().__init__()
+        self.context_length=context_length
+        if isinstance(vision_layers,(tuple,list)):
+            vision_heads=vision_width*32//64
+            self.visual=ModifiedResNet(
+                layers=vision_layers,
+                output_dim=embed_dim,
+                heads=vision_heads,
+                input_resolution=image_resolution,
+                width=vision_width
+            )
+        else:
+            vision_heads=vision_width//64
+            self.visual=VisionTransformer64(
+                input_resolution=image_resolution,
+                patch_size=vision_patch_size,
+                width=vision_width,
+                layers=vision_layers,
+                heads=vision_heads,
+                output_dim=embed_dim
+            )
+        self.transformer=Transformer64(
+            width=transformer_width,
+            layers=transformer_layers,
+            heads=transformer_heads,
+            attn_mask=self.build_attention_mask()
+        )
+        self.vocab_size=vocab_size
+        self.token_embedding=nn.Embedding(vocab_size,transformer_width)
+        self.positional_embedding=nn.Parameter(torch.empty(self.context_length,transformer_width))
+        self.ln_final=LayerNorm64(transformer_width)
+        self.text_projection=nn.Parameter(torch.empty(transformer_width,embed_dim))
+        self.logit_scale=nn.Parameter(torch.ones([])*np.log(1/0.07))
+        self.initialize_parameters()
+    def initialize_parameters(self):
+        nn.init.normal_(self.token_embedding.weight,std=0.02)
+        nn.init.normal_(self.positional_embedding,std=0.01)
+        if isinstance(self.visual,ModifiedResNet):
+            if self.visual.attnpool is not None:
+                std=self.visual.attnpool.c_proj.in_features ** -0.5
+                nn.init.normal_(self.visual.attnpool.q_proj.weight,std=std)
+                nn.init.normal_(self.visual.attnpool.k_proj.weight,std=std)
+                nn.init.normal_(self.visual.attnpool.v_proj.weight,std=std)
+                nn.init.normal_(self.visual.attnpool.c_proj.weight,std=std)
+            for resnet_block in [self.visual.layer1,self.visual.layer2,self.visual.layer3,self.visual.layer4]:
+                for name,param in resnet_block.named_parameters():
+                    if name.endswith("bn3.weight"):
+                        nn.init.zeros_(param)
+        proj_std=(self.transformer.width**-0.5)*((2*self.transformer.layers)**-0.5)
+        attn_std=self.transformer.width**-0.5
+        fc_std=(2*self.transformer.width)**-0.5
+        for block in self.transformer.resblocks:
+            nn.init.normal_(block.attn.in_proj_weight,std=attn_std)
+            nn.init.normal_(block.attn.out_proj.weight,std=proj_std)
+            nn.init.normal_(block.mlp.c_fc.weight,std=fc_std)
+            nn.init.normal_(block.mlp.c_proj.weight,std=proj_std)
+        if self.text_projection is not None:
+            nn.init.normal_(self.text_projection,std=self.transformer.width**-0.5)
+    def build_attention_mask(self):
+        mask=torch.empty(self.context_length,self.context_length)
+        mask.fill_(float("-inf"))
+        mask.triu_(1)
+        return mask
+    @property
+    def dtype(self):
+        return self.visual.conv1.weight.dtype
+    def encode_image(self,image):
+        return self.visual(image.type(self.dtype))
+    def encode_text(self,text):
+        x=self.token_embedding(text).type(self.dtype)
+        x=x + self.positional_embedding.type(self.dtype)
+        x=x.permute(1,0,2)  # NLD -> LND
+        x=self.transformer(x)
+        x=x.permute(1,0,2)  # LND -> NLD
+        x=self.ln_final(x).type(self.dtype)
+        x=x[torch.arange(x.shape[0]),text.argmax(dim=-1)] @ self.text_projection
+        return x
+    def forward(self,image,text):
+        image_features=self.encode_image(image)
+        text_features=self.encode_text(text)
+        image_features=image_features/image_features.norm(dim=1,keepdim=True)
+        text_features=text_features/text_features.norm(dim=1,keepdim=True)
+        logit_scale=self.logit_scale.exp()
+        logits_per_image=logit_scale * image_features @ text_features.t()
+        logits_per_text=logits_per_image.t()
+        return logits_per_image,logits_per_text
+
 def convert_weights(model:nn.Module):
     def _convert_weights_to_fp16(l):
         if isinstance(l,(nn.Conv1d,nn.Conv2d,nn.Linear)):
@@ -462,6 +621,8 @@ def build_model(fp16bit,state_dict: dict):
     transformer_layers=len(set(k.split(".")[2] for k in state_dict if k.startswith(f"transformer.resblocks")))
     if fp16bit==True:
         model=CLIP16(embed_dim,image_resolution,vision_layers,vision_width,vision_patch_size,context_length,vocab_size,transformer_width,transformer_heads,transformer_layers)
+    elif fp64bit==True:
+        model=CLIP64(embed_dim,image_resolution,vision_layers,vision_width,vision_patch_size,context_length,vocab_size,transformer_width,transformer_heads,transformer_layers)
     else:    
         model=CLIP(embed_dim,image_resolution,vision_layers,vision_width,vision_patch_size,context_length,vocab_size,transformer_width,transformer_heads,transformer_layers)
     for key in ["input_resolution","context_length","vocab_size"]:
