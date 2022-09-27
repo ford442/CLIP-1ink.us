bfcabe300b54f769a8a8388cda4afdd70d6ca66a
Noah Cohn <noahc42@gmail.com>
2022-09-27 00:57:28 -0700

    Update model.py

--- clip/model.py
+++ clip/model.py
@@ -38,7 +38,7 @@ class Bottleneck(nn.Module):
 class AttentionPool2d(nn.Module):
     def __init__(self,spacial_dim: int,embed_dim: int,num_heads: int,output_dim: int=None):
         super().__init__()
-        self.positional_embedding=nn.Parameter(torch.randn(spacial_dim**2+1,embed_dim)/embed_dim**0.5,device=torch.device("cuda:0"))
+        self.positional_embedding=nn.Parameter(torch.randn(spacial_dim**2+1,embed_dim,device=torch.device("cuda:0"))/embed_dim**0.5)
         self.k_proj=nn.Linear(embed_dim,embed_dim)
         self.q_proj=nn.Linear(embed_dim,embed_dim)
         self.v_proj=nn.Linear(embed_dim,embed_dim)
